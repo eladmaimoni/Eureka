42a86f65a14af899e5fdf01b6e0443100081c80a
Elad Maimoni <elad.maimoni@r-go.io>
2022-06-15 21:44:35 +0300

    perspective test passes

--- Eureka.Graphics/Camera.cpp
+++ Eureka.Graphics/Camera.cpp
@@ -4,4 +4,39 @@
 namespace eureka
 {
 
+    void PerspectiveCamera::SetPosition(const Eigen::Vector3f& position)
+    {
+
+    }
+
+    void PerspectiveCamera::SetLookDirection(const Eigen::Vector3f& direction)
+    {
+
+    }
+
+    void PerspectiveCamera::SetDirectionRelativeToBase(const Eigen::Vector3f& baseDirection, const Eigen::Vector3f& xyzRotationDegrees)
+    {
+
+    }
+
+    void PerspectiveCamera::SetFullViewport(float topLeftX, float topLeftY, float width, float height)
+    {
+
+    }
+
+    void PerspectiveCamera::SetNearFar(float nearVal, float farVal)
+    {
+
+    }
+
+    void PerspectiveCamera::SetVerticalFov(float verticalFovRadians)
+    {
+
+    }
+
+    void PerspectiveCamera::SetHorizontalFov(float horizontalFovRadians)
+    {
+
+    }
+
 }
