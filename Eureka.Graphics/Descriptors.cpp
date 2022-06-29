6d5a0ab8823ddca6b1b9c34f10b81f28e9909d26
Elad Maimoni <elad.maimoni@r-go.io>
2022-06-29 22:16:21 +0300

    wip

--- Eureka.Graphics/Descriptors.cpp
+++ Eureka.Graphics/Descriptors.cpp
@@ -56,4 +56,61 @@ namespace eureka
 
         return vkr::DescriptorSet(*_device, descriptorSet, *_pool);
     }
+
+    PerViewDescriptorSetLayout::PerViewDescriptorSetLayout(DeviceContext& deviceContext)
+    {
+        // describe the relation between the shader indices (set 0, binding 0)
+        // to the host indices 
+        // - host indices are the sets
+        // - device indices are the set index and binding number within the set that can potentially be unordered
+        // set 0, only has a single binding inside shader
+        vk::DescriptorSetLayoutBinding descriptorSetLayoutBinding
+        {
+            .binding = 0, // shader side index (why not named location??)
+            .descriptorType = vk::DescriptorType::eUniformBuffer,
+            .descriptorCount = 1, // a single constant buffer
+            .stageFlags = vk::ShaderStageFlagBits::eVertex
+        };
+
+        vk::DescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo
+        {
+            .bindingCount = 1,
+            .pBindings = &descriptorSetLayoutBinding
+        };
+
+        _descriptorSetLayout = deviceContext.LogicalDevice()->createDescriptorSetLayout(descriptorSetLayoutCreateInfo);
+    }
+
+
+
+
+    PerNormalMappedModelDescriptorSetLayout::PerNormalMappedModelDescriptorSetLayout(DeviceContext& deviceContext)
+    {
+        std::array<vk::DescriptorSetLayoutBinding, 2> descriptorSetLayoutBindings
+        {
+            vk::DescriptorSetLayoutBinding
+            {
+            .binding = 0, // shader side index (why not named location??)
+            .descriptorType = vk::DescriptorType::eUniformBuffer,
+            .descriptorCount = 1, // a single constant buffer
+            .stageFlags = vk::ShaderStageFlagBits::eVertex
+            },
+            vk::DescriptorSetLayoutBinding
+            {
+            .binding = 0, // shader side index (why not named location??)
+            .descriptorType = vk::DescriptorType::eUniformBuffer,
+            .descriptorCount = 1, // a single constant buffer
+            .stageFlags = vk::ShaderStageFlagBits::eVertex
+            }
+        };
+
+        vk::DescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo
+        {
+            .bindingCount = static_cast<uint32_t>(descriptorSetLayoutBindings.size()),
+            .pBindings = descriptorSetLayoutBindings.data()
+        };
+
+        _descriptorSetLayout = deviceContext.LogicalDevice()->createDescriptorSetLayout(descriptorSetLayoutCreateInfo);
+    }
+
 }
--- Eureka.Graphics/Pipeline.cpp
+++ Eureka.Graphics/Pipeline.cpp
@@ -96,30 +96,6 @@ namespace eureka
         return meshPipelinePreset;
     }
 
-    PerViewDescriptorSetLayout::PerViewDescriptorSetLayout(DeviceContext& deviceContext)
-    {
-        // describe the relation between the shader indices (set 0, binding 0)
-        // to the host indices 
-        // - host indices are the sets
-        // - device indices are the set index and binding number within the set that can potentially be unordered
-        // set 0, only has a single binding inside shader
-        vk::DescriptorSetLayoutBinding descriptorSetLayoutBinding
-        {
-            .binding = 0, // shader side index (why not named location??)
-            .descriptorType = vk::DescriptorType::eUniformBuffer,
-            .descriptorCount = 1, // a single constant buffer
-            .stageFlags = vk::ShaderStageFlagBits::eVertex
-        };
-
-        vk::DescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo
-        {
-            .bindingCount = 1,
-            .pBindings = &descriptorSetLayoutBinding
-        };
-
-        _descriptorSetLayout = deviceContext.LogicalDevice()->createDescriptorSetLayout(descriptorSetLayoutCreateInfo);
-    }
-
 
 
     //////////////////////////////////////////////////////////////////////////
