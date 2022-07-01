36cdcbd22e83d155e431ba300f12c7bb0c5aad5c
Elad Maimoni <elad.maimoni@r-go.io>
2022-07-01 12:24:45 +0300

    fixed hlsl compilation on vs

--- Eureka.Graphics/Camera.hpp
+++ Eureka.Graphics/Camera.hpp
@@ -12,15 +12,15 @@ namespace eureka
     class PerspectiveCamera
     {
     private:
-        float           _fovY{PERSPECTIVE_CAMERA_DEFAULT_FOV};
-        float           _zNear{0.01f};
-        float           _zFar{1000.0f};
-        Eigen::Vector3f _up{0.0f, 1.0f, 0.0f};
-        Eigen::Vector3f _position{0.0f, 0.0f, 10.0f};
-        Eigen::Vector3f _direction{0.0f, 0.0f, -1.0f};
-
-        ViewProjection  _viewProjection;
-        vk::Viewport    _viewport{.x = 0.0f, .y = 0.0f, .width = 100.0f, .height = 100.0f, .minDepth = 0.0f, .maxDepth = 1.0f };
+        float                                            _fovY{PERSPECTIVE_CAMERA_DEFAULT_FOV};
+        float                                            _zNear{0.01f};
+        float                                            _zFar{1000.0f};
+        Eigen::Vector3f                                  _up{0.0f, 1.0f, 0.0f};
+        Eigen::Vector3f                                  _position{0.0f, 0.0f, 10.0f};
+        Eigen::Vector3f                                  _direction{0.0f, 0.0f, -1.0f};
+                                                         
+        ViewProjection                                   _viewProjection;
+        vk::Viewport                                     _viewport{.x = 0.0f, .y = 0.0f, .width = 100.0f, .height = 100.0f, .minDepth = 0.0f, .maxDepth = 1.0f };
 
         HostVisibleDeviceConstantBuffer                   _constantBuffer;
         std::shared_ptr<SubmissionThreadExecutionContext> _submissionThreadExecutionContext;
