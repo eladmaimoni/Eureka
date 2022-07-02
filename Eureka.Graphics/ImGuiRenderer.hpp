005528011298c3aaabfc2fe3b631f07d48c20f68
Elad Maimoni <elad.maimoni@r-go.io>
2022-07-02 15:30:03 +0300

    wip

---
+++ Eureka.Graphics/ImGuiRenderer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <imgui.h>
+#include <Buffer.hpp>
+
+
+namespace eureka
+{
+    
+
+    class ImGuiRenderer
+    {
+
+        ImGuiRenderer(DeviceContext& deviceContext)
+        {
+
+        }
+
+        void SyncBuffers()
+        {
+            ImDrawData* imDrawData = ImGui::GetDrawData();
+
+            VkDeviceSize vertexBufferSize = imDrawData->TotalVtxCount * sizeof(ImDrawVert);
+            VkDeviceSize indexBufferSize = imDrawData->TotalIdxCount * sizeof(ImDrawIdx);
+
+            if ((vertexBufferSize == 0) || (indexBufferSize == 0)) 
+            {
+                return;
+            }
+        }
+    };
+
+}
+
