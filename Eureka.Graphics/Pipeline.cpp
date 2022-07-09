0050f808534c327c10620e15de41173ebd343c71
Elad Maimoni <elad.maimoni@r-go.io>
2022-07-09 12:10:30 +0300

    refactor pipeline presets

--- Eureka.Graphics/Pipeline.cpp
+++ Eureka.Graphics/Pipeline.cpp
@@ -1,124 +1,55 @@
 #include "Pipeline.hpp"
 #include "PipelineTypes.hpp"
 #include <ShadersCache.hpp>
-
+#include "PipelineHelpers.hpp"
 
 namespace eureka
 {
-    struct FixedPiplinePreset
-    {
-        vk::PipelineInputAssemblyStateCreateInfo input_assembly_create_info;
-        vk::PipelineRasterizationStateCreateInfo rasterization_state_create_info;
-        vk::PipelineColorBlendStateCreateInfo    color_blend_state_create_info;
-        vk::PipelineViewportStateCreateInfo      viewport_state_create_info;
-        vk::PipelineDynamicStateCreateInfo       dynamic_state_create_info;
-        vk::PipelineDepthStencilStateCreateInfo  depth_stencil_state_create_info;
-        vk::PipelineMultisampleStateCreateInfo   multisampling_state_create_info;
-    };
-
-    struct MeshPipelinePreset
-    {
-        vk::PipelineColorBlendAttachmentState color_blend_attachment_state;
-        std::array<vk::DynamicState, 2>       enabled_dynamic_states{ vk::DynamicState::eViewport, vk::DynamicState::eScissor };
-        FixedPiplinePreset                    fixed_preset;
-
-        void Setup()
-        {
-
-        }
-    };
-
-    struct MeshFixedPresetTraits
-    {
-        static constexpr vk::CullModeFlags cull_mode = vk::CullModeFlagBits::eNone;
-        static constexpr bool depth_test = true;
-        static constexpr bool depth_write = true;
-    };
-    struct UIFixedPresetTraits
-    {
-        static constexpr vk::CullModeFlags cull_mode = vk::CullModeFlagBits::eNone;
-        static constexpr bool depth_test = false;
-        static constexpr bool depth_write = false;
-    };
-    template<typename Traits>
-    void SetupFixedPreset(MeshPipelinePreset& meshPipelinePreset)
-    {
-        meshPipelinePreset.fixed_preset = FixedPiplinePreset
-        {
-            .input_assembly_create_info = vk::PipelineInputAssemblyStateCreateInfo
-            {
-                .topology = vk::PrimitiveTopology::eTriangleList
-            },
-            .rasterization_state_create_info = vk::PipelineRasterizationStateCreateInfo
-            {
-                .depthClampEnable = false,
-                .rasterizerDiscardEnable = false,
-                .polygonMode = vk::PolygonMode::eFill,
-                .cullMode = Traits::cull_mode,
-                .frontFace = vk::FrontFace::eCounterClockwise,
-                .depthBiasEnable = false,
-                .lineWidth = 1.0f
-            },
-            .color_blend_state_create_info = vk::PipelineColorBlendStateCreateInfo
-            {
-                .attachmentCount = 1,
-                .pAttachments = &meshPipelinePreset.color_blend_attachment_state
-            },
-            .viewport_state_create_info = vk::PipelineViewportStateCreateInfo
-            {
-                .viewportCount = 1,
-                .scissorCount = 1
-            },
-            .dynamic_state_create_info = vk::PipelineDynamicStateCreateInfo
-            {
-                .dynamicStateCount = static_cast<uint32_t>(meshPipelinePreset.enabled_dynamic_states.size()),
-                .pDynamicStates = meshPipelinePreset.enabled_dynamic_states.data()
-            },
-            .depth_stencil_state_create_info = vk::PipelineDepthStencilStateCreateInfo
-            {
-                .depthTestEnable = Traits::depth_test,
-                .depthWriteEnable = Traits::depth_write,
-                .depthCompareOp = vk::CompareOp::eLessOrEqual,
-                .depthBoundsTestEnable = false,
-                .stencilTestEnable = false,
-                .front = vk::StencilOpState{.failOp = vk::StencilOp::eKeep,.passOp = vk::StencilOp::eKeep, .depthFailOp = vk::StencilOp::eKeep, .compareOp = vk::CompareOp::eAlways },
-                .back = vk::StencilOpState{.failOp = vk::StencilOp::eKeep,.passOp = vk::StencilOp::eKeep, .depthFailOp = vk::StencilOp::eKeep, .compareOp = vk::CompareOp::eAlways }
-            },
-            .multisampling_state_create_info = vk::PipelineMultisampleStateCreateInfo
-            {
-                .rasterizationSamples = vk::SampleCountFlagBits::e1,
-                .pSampleMask = nullptr
-            }
-        };
-    }
-
-    MeshPipelinePreset CreateMeshPipelinePreset()
-    {
-        MeshPipelinePreset meshPipelinePreset
-        {
-            .color_blend_attachment_state = vk::PipelineColorBlendAttachmentState
-            {
-                .blendEnable = false,
-                .colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA
-            },
-            .enabled_dynamic_states{ vk::DynamicState::eViewport, vk::DynamicState::eScissor }
-        };
-
-        return meshPipelinePreset;
-    }
-
     //////////////////////////////////////////////////////////////////////////
     //
     //                        UIPipeline
     // 
     //////////////////////////////////////////////////////////////////////////
 
+
+    ImGuiPipeline::ImGuiPipeline(
+        DeviceContext& deviceContext,
+        const DepthColorRenderPass& renderPass,
+        const SingleFragmentShaderCombinedImageSamplerDescriptorSetLayout& fragmentShaderSetLayout
+    )
+    {
+        std::array<vk::DescriptorSetLayout, 1> setLayouts
+        {
+            fragmentShaderSetLayout.Get(),
+        };
+
+        vk::PushConstantRange pushConstantsRange
+        {
+            .stageFlags = vk::ShaderStageFlagBits::eVertex,
+            .offset = 0,
+            .size = sizeof(ImGuiPushConstantsBlock)
+        };
+
+
+        vk::PipelineLayoutCreateInfo pipelineLayoutCreateInfo
+        {
+            .setLayoutCount = static_cast<uint32_t>(setLayouts.size()),
+            .pSetLayouts = setLayouts.data(),
+            .pushConstantRangeCount = 1,
+            .pPushConstantRanges = &pushConstantsRange
+        };
+
+        _pipelineLayout = deviceContext.LogicalDevice()->createPipelineLayout(pipelineLayoutCreateInfo);
+
+        Setup(deviceContext, renderPass.Get());
+    }
+
     void ImGuiPipeline::Setup(DeviceContext& deviceContext, vk::RenderPass renderPass)
     {
         //
         // Vertex Attributes 
         //
-        
+
         vk::VertexInputBindingDescription vertexInputBinding
         {
             .binding = 0,
@@ -161,7 +92,7 @@ namespace eureka
             .pVertexAttributeDescriptions = vertexInputAttributs.data()
         };
 
-        auto preset = CreateMeshPipelinePreset();
+        DefaultFixedPiplinePreset preset{};
         SetupFixedPreset<UIFixedPresetTraits>(preset);
 
         //
@@ -197,14 +128,14 @@ namespace eureka
             .stageCount = static_cast<uint32_t>(shaderStages.size()),
             .pStages = shaderStages.data(),
             .pVertexInputState = &vertexInputState,
-            .pInputAssemblyState = &preset.fixed_preset.input_assembly_create_info,
+            .pInputAssemblyState = &preset.input_assembly_create_info,
             .pTessellationState = nullptr,
-            .pViewportState = &preset.fixed_preset.viewport_state_create_info,
-            .pRasterizationState = &preset.fixed_preset.rasterization_state_create_info,
-            .pMultisampleState = &preset.fixed_preset.multisampling_state_create_info,
-            .pDepthStencilState = &preset.fixed_preset.depth_stencil_state_create_info,
-            .pColorBlendState = &preset.fixed_preset.color_blend_state_create_info,
-            .pDynamicState = &preset.fixed_preset.dynamic_state_create_info,
+            .pViewportState = &preset.viewport_state_create_info,
+            .pRasterizationState = &preset.rasterization_state_create_info,
+            .pMultisampleState = &preset.multisampling_state_create_info,
+            .pDepthStencilState = &preset.depth_stencil_state_create_info,
+            .pColorBlendState = &preset.color_blend_state_create_info,
+            .pDynamicState = &preset.dynamic_state_create_info,
             .layout = *_pipelineLayout,
             .renderPass = renderPass
         };
@@ -213,38 +144,6 @@ namespace eureka
             deviceContext.Shaders()->Cache(),
             pipelineCreateInfo
         );
-    }
-
-    ImGuiPipeline::ImGuiPipeline(
-        DeviceContext& deviceContext,
-        const DepthColorRenderPass& renderPass,
-        const SingleFragmentShaderCombinedImageSamplerDescriptorSetLayout& fragmentShaderSetLayout
-    )
-    {
-        std::array<vk::DescriptorSetLayout, 1> setLayouts
-        {
-            fragmentShaderSetLayout.Get(),
-        };
-
-        vk::PushConstantRange pushConstantsRange
-        {
-            .stageFlags = vk::ShaderStageFlagBits::eVertex,
-            .offset = 0,
-            .size = sizeof(ImGuiPushConstantsBlock)
-        };
-
-
-        vk::PipelineLayoutCreateInfo pipelineLayoutCreateInfo
-        {
-            .setLayoutCount = static_cast<uint32_t>(setLayouts.size()),
-            .pSetLayouts = setLayouts.data(),
-            .pushConstantRangeCount = 1,
-            .pPushConstantRanges = &pushConstantsRange
-        };
-
-        _pipelineLayout = deviceContext.LogicalDevice()->createPipelineLayout(pipelineLayoutCreateInfo);
-
-        Setup(deviceContext, renderPass.Get());
     }
 
     //////////////////////////////////////////////////////////////////////////
@@ -311,7 +210,7 @@ namespace eureka
             .pVertexAttributeDescriptions = vertexInputAttributs.data()
         };
 
-        auto preset = CreateMeshPipelinePreset();
+        DefaultFixedPiplinePreset preset{};
         SetupFixedPreset<MeshFixedPresetTraits>(preset);
 
         //
@@ -347,14 +246,14 @@ namespace eureka
             .stageCount = static_cast<uint32_t>(shaderStages.size()),
             .pStages = shaderStages.data(),
             .pVertexInputState = &vertexInputState,
-            .pInputAssemblyState = &preset.fixed_preset.input_assembly_create_info,
+            .pInputAssemblyState = &preset.input_assembly_create_info,
             .pTessellationState = nullptr,
-            .pViewportState = &preset.fixed_preset.viewport_state_create_info,
-            .pRasterizationState = &preset.fixed_preset.rasterization_state_create_info,
-            .pMultisampleState = &preset.fixed_preset.multisampling_state_create_info,
-            .pDepthStencilState = &preset.fixed_preset.depth_stencil_state_create_info,
-            .pColorBlendState = &preset.fixed_preset.color_blend_state_create_info,
-            .pDynamicState = &preset.fixed_preset.dynamic_state_create_info,
+            .pViewportState = &preset.viewport_state_create_info,
+            .pRasterizationState = &preset.rasterization_state_create_info,
+            .pMultisampleState = &preset.multisampling_state_create_info,
+            .pDepthStencilState = &preset.depth_stencil_state_create_info,
+            .pColorBlendState = &preset.color_blend_state_create_info,
+            .pDynamicState = &preset.dynamic_state_create_info,
             .layout = *_pipelineLayout,
             .renderPass = renderPass
         };
@@ -373,16 +272,13 @@ namespace eureka
     // 
     //////////////////////////////////////////////////////////////////////////
 
-
     PhongShadedMeshWithNormalMapPipeline::PhongShadedMeshWithNormalMapPipeline(
         DeviceContext& deviceContext, 
         const DepthColorRenderPass& renderPass,
         const SingleVertexShaderUBODescriptorSetLayout& perViewDescriptorSetLayout,
         const ColorAndNormalMapFragmentDescriptorSetLayout& perNormalMappedModelDescriptorSetLayout
     )
-    {
-   
-
+    { 
         std::array<vk::DescriptorSetLayout, 2> setLayouts
         {
             perViewDescriptorSetLayout.Get(),
@@ -488,7 +384,7 @@ namespace eureka
             .pVertexAttributeDescriptions = vertexInputAttributs.data()
         };
 
-        auto preset = CreateMeshPipelinePreset();
+        DefaultFixedPiplinePreset preset{};
         SetupFixedPreset<MeshFixedPresetTraits>(preset);
 
         //
@@ -524,14 +420,14 @@ namespace eureka
             .stageCount = static_cast<uint32_t>(shaderStages.size()),
             .pStages = shaderStages.data(),
             .pVertexInputState = &vertexInputState,
-            .pInputAssemblyState = &preset.fixed_preset.input_assembly_create_info,
+            .pInputAssemblyState = &preset.input_assembly_create_info,
             .pTessellationState = nullptr,
-            .pViewportState = &preset.fixed_preset.viewport_state_create_info,
-            .pRasterizationState = &preset.fixed_preset.rasterization_state_create_info,
-            .pMultisampleState = &preset.fixed_preset.multisampling_state_create_info,
-            .pDepthStencilState = &preset.fixed_preset.depth_stencil_state_create_info,
-            .pColorBlendState = &preset.fixed_preset.color_blend_state_create_info,
-            .pDynamicState = &preset.fixed_preset.dynamic_state_create_info,
+            .pViewportState = &preset.viewport_state_create_info,
+            .pRasterizationState = &preset.rasterization_state_create_info,
+            .pMultisampleState = &preset.multisampling_state_create_info,
+            .pDepthStencilState = &preset.depth_stencil_state_create_info,
+            .pColorBlendState = &preset.color_blend_state_create_info,
+            .pDynamicState = &preset.dynamic_state_create_info,
             .layout = *_pipelineLayout,
             .renderPass = renderPass
         };
--- Eureka.Graphics/PipelineHelpers.hpp
+++ Eureka.Graphics/PipelineHelpers.hpp
@@ -2,7 +2,133 @@
 
 namespace eureka
 {
+    struct SingleColorOutputAttachmentPreset
+    {
+        std::array<vk::PipelineColorBlendAttachmentState, 1> color_blend_attachment_state
+        { 
+            vk::PipelineColorBlendAttachmentState
+            {
+                .blendEnable = false,
+                .colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA
+            }
+        };
+    };
+
+    struct DynamicViewprtScissorPreset
+    {
+        std::array<vk::DynamicState, 2> enabled_dynamic_states{ vk::DynamicState::eViewport, vk::DynamicState::eScissor };
+    };
+
+    template<typename ColorBlendAttachmentPreset, typename DynamicStatePreset>
+    struct FixedPiplinePreset
+    {
+        vk::PipelineInputAssemblyStateCreateInfo input_assembly_create_info;
+        vk::PipelineRasterizationStateCreateInfo rasterization_state_create_info;
+        vk::PipelineColorBlendStateCreateInfo    color_blend_state_create_info;
+        vk::PipelineViewportStateCreateInfo      viewport_state_create_info;
+        vk::PipelineDynamicStateCreateInfo       dynamic_state_create_info;
+        vk::PipelineDepthStencilStateCreateInfo  depth_stencil_state_create_info;
+        vk::PipelineMultisampleStateCreateInfo   multisampling_state_create_info;
+        ColorBlendAttachmentPreset               color_blend_preset;
+        DynamicStatePreset                       dynamic_state_preset;
+    };
+
+    using DefaultFixedPiplinePreset = FixedPiplinePreset<SingleColorOutputAttachmentPreset, DynamicViewprtScissorPreset>;
+
+    //struct MeshPipelinePreset
+    //{
+    //    vk::PipelineColorBlendAttachmentState color_blend_attachment_state;
+    //    std::array<vk::DynamicState, 2>       enabled_dynamic_states{ vk::DynamicState::eViewport, vk::DynamicState::eScissor };
+    //    FixedPiplinePreset                    fixed_preset;
+    //};
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    //                        FixedPresetTraits       
+    //
+    //////////////////////////////////////////////////////////////////////////
+    struct MeshFixedPresetTraits
+    {
+        static constexpr vk::CullModeFlags cull_mode = vk::CullModeFlagBits::eNone;
+        static constexpr bool depth_test = true;
+        static constexpr bool depth_write = true;
+    };
+    struct UIFixedPresetTraits
+    {
+        static constexpr vk::CullModeFlags cull_mode = vk::CullModeFlagBits::eNone;
+        static constexpr bool depth_test = false;
+        static constexpr bool depth_write = false;
+    };
+
+    //////////////////////////////////////////////////////////////////////////
+    //
+    //                        SetupFixedPreset       
+    //
+    //////////////////////////////////////////////////////////////////////////
+    template< typename Traits, typename FixedPreset>
+    void SetupFixedPreset(FixedPreset& meshPipelinePreset)
+    {
+        meshPipelinePreset.input_assembly_create_info = vk::PipelineInputAssemblyStateCreateInfo
+        {
+            .topology = vk::PrimitiveTopology::eTriangleList
+        };
+        meshPipelinePreset.rasterization_state_create_info = vk::PipelineRasterizationStateCreateInfo
+        {
+            .depthClampEnable = false,
+            .rasterizerDiscardEnable = false,
+            .polygonMode = vk::PolygonMode::eFill,
+            .cullMode = Traits::cull_mode,
+            .frontFace = vk::FrontFace::eCounterClockwise,
+            .depthBiasEnable = false,
+            .lineWidth = 1.0f
+        };
+        meshPipelinePreset.color_blend_state_create_info = vk::PipelineColorBlendStateCreateInfo
+        {
+            .attachmentCount = static_cast<uint32_t>(meshPipelinePreset.color_blend_preset.color_blend_attachment_state.size()),
+            .pAttachments = meshPipelinePreset.color_blend_preset.color_blend_attachment_state.data()
+        };
+        meshPipelinePreset.viewport_state_create_info = vk::PipelineViewportStateCreateInfo
+        {
+            .viewportCount = 1,
+            .scissorCount = 1
+        };
+        meshPipelinePreset.dynamic_state_create_info = vk::PipelineDynamicStateCreateInfo
+        {
+            .dynamicStateCount = static_cast<uint32_t>(meshPipelinePreset.dynamic_state_preset.enabled_dynamic_states.size()),
+            .pDynamicStates = meshPipelinePreset.dynamic_state_preset.enabled_dynamic_states.data()
+        };
+        meshPipelinePreset.depth_stencil_state_create_info = vk::PipelineDepthStencilStateCreateInfo
+        {
+            .depthTestEnable = Traits::depth_test,
+            .depthWriteEnable = Traits::depth_write,
+            .depthCompareOp = vk::CompareOp::eLessOrEqual,
+            .depthBoundsTestEnable = false,
+            .stencilTestEnable = false,
+            .front = vk::StencilOpState{.failOp = vk::StencilOp::eKeep,.passOp = vk::StencilOp::eKeep, .depthFailOp = vk::StencilOp::eKeep, .compareOp = vk::CompareOp::eAlways },
+            .back = vk::StencilOpState{.failOp = vk::StencilOp::eKeep,.passOp = vk::StencilOp::eKeep, .depthFailOp = vk::StencilOp::eKeep, .compareOp = vk::CompareOp::eAlways }
+        };
+        meshPipelinePreset.multisampling_state_create_info = vk::PipelineMultisampleStateCreateInfo
+        {
+            .rasterizationSamples = vk::SampleCountFlagBits::e1,
+            .pSampleMask = nullptr
+        };
+        
+    }
 
 
+    //inline MeshPipelinePreset CreateMeshPipelinePreset()
+    //{
+    //    MeshPipelinePreset meshPipelinePreset
+    //    {
+    //        .color_blend_attachment_state = vk::PipelineColorBlendAttachmentState
+    //        {
+    //            .blendEnable = false,
+    //            .colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA
+    //        },
+    //        .enabled_dynamic_states{ vk::DynamicState::eViewport, vk::DynamicState::eScissor }
+    //    };
+
+    //    return meshPipelinePreset;
+    //}
 }
 
