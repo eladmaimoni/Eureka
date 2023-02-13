7f889776be4d2fd3051bbf904a2ef3269bba7aa8
Elad Maimoni <mr.maimon@gmail.com>
2023-02-13 16:27:53 +0200

    cmake - cleaner dll copy to output directory

--- utility.cmake
+++ utility.cmake
@@ -20,32 +20,36 @@ function(link_library_and_copy_dynamic_library_to_output_path target_name dynami
 	
 	set(IS_DEBUG_VARIANT $<CONFIG:Debug>) # An abbreviation to make the example easier to read.
 	set(IS_RELEASE_VARIANT $<NOT:$<CONFIG:Debug>>) # An abbreviation to make the example easier to read.
+	#add_custom_command(
+	#	TARGET ${target_name} POST_BUILD
+	#	COMMAND "${CMAKE_COMMAND}" -E
+	#	# in release build, this is an echo command
+	#	$<${IS_RELEASE_VARIANT}:echo>
+	#	# output text to make clear that the copy command is not actually issued
+	#	# in release build, the echo is 
+	#	$<${IS_RELEASE_VARIANT}:"copy omitted for release build, command would have been ">
+	#	# the actual copy command, which is overridden by the "echo" above
+	#	# in the case of a non-release build
+	#	copy_if_different ${dynamic_library_target_imported_location_debug} $<TARGET_FILE_DIR:${target_name}>
+	#	)
+	#
+	#add_custom_command(
+	#	TARGET ${target_name} POST_BUILD
+	#	COMMAND "${CMAKE_COMMAND}" -E
+	#	# in release build, this is an echo command
+	#	$<${IS_DEBUG_VARIANT}:echo>
+	#	# output text to make clear that the copy command is not actually issued
+	#	# in release build, the echo is 
+	#	$<${IS_DEBUG_VARIANT}:"copy omitted for debug build, command would have been ">
+	#	# the actual copy command, which is overridden by the "echo" above
+	#	# in the case of a non-release build
+	#	copy_if_different ${dynamic_library_target_imported_location_release} $<TARGET_FILE_DIR:${target_name}>
+	#)
 	add_custom_command(
 		TARGET ${target_name} POST_BUILD
 		COMMAND "${CMAKE_COMMAND}" -E
-		# in release build, this is an echo command
-		$<${IS_RELEASE_VARIANT}:echo>
-		# output text to make clear that the copy command is not actually issued
-		# in release build, the echo is 
-		$<${IS_RELEASE_VARIANT}:"copy omitted for release build, command would have been ">
-		# the actual copy command, which is overridden by the "echo" above
-		# in the case of a non-release build
-		copy_if_different ${dynamic_library_target_imported_location_debug} $<TARGET_FILE_DIR:${target_name}>
-		)
-	
-	add_custom_command(
-		TARGET ${target_name} POST_BUILD
-		COMMAND "${CMAKE_COMMAND}" -E
-		# in release build, this is an echo command
-		$<${IS_DEBUG_VARIANT}:echo>
-		# output text to make clear that the copy command is not actually issued
-		# in release build, the echo is 
-		$<${IS_DEBUG_VARIANT}:"copy omitted for debug build, command would have been ">
-		# the actual copy command, which is overridden by the "echo" above
-		# in the case of a non-release build
-		copy_if_different ${dynamic_library_target_imported_location_release} $<TARGET_FILE_DIR:${target_name}>
+		copy_if_different $<IF:${IS_DEBUG_VARIANT},${dynamic_library_target_imported_location_debug},${dynamic_library_target_imported_location_release}> $<TARGET_FILE_DIR:${target_name}>
 	)
-	
 	#if(MSVC)
 	#	add_custom_command(TARGET ${target_name} PRE_BUILD
 	#		COMMAND cmd.exe if "$(Configuration)" == "Debug" "${CMAKE_COMMAND}" -E copy_if_different "${dynamic_library_target_imported_location_debug}" "$<TARGET_FILE_DIR:${target_name}>" )
