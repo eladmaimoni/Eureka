3d4ab48b0a9a175f4331a623e171d6e0f0add608
Elad Maimoni <mr.maimon@gmail.com>
2023-02-18 21:44:03 +0200

    clang-tidy - fixed some problematic macros

--- Libs/Eureka.Core/compiler.hpp
+++ Libs/Eureka.Core/compiler.hpp
@@ -1,13 +1,13 @@
 
-
+// NOLINTBEGIN(*)
 
 #if defined(_MSC_VER) && _MSC_VER >= 1400
 #define EUREKA_MSVC_WARNING_PUSH      __pragma(warning( push ))
 #define EUREKA_MSVC_WARNING_DISABLE(...) \
-__pragma(warning(disable: ##__VA_ARGS__))
+__pragma(warning(disable : __VA_ARGS__))
 #define EUREKA_MSVC_WARNING_PUSH_DISABLE(...) \
 EUREKA_MSVC_WARNING_PUSH \
-EUREKA_MSVC_WARNING_DISABLE(##__VA_ARGS__)
+EUREKA_MSVC_WARNING_DISABLE(__VA_ARGS__)
 #define EUREKA_MSVC_WARNING_SUPPRESS(...) __pragma(warning(suppress:##__VA_ARGS__))
 #define EUREKA_MSVC_WARNING_POP __pragma(warning(pop))
 #define EUREKA_DISABLE_OPTIMIZATIONS() __pragma( optimize( "", off ) )
@@ -21,3 +21,5 @@ EUREKA_MSVC_WARNING_DISABLE(##__VA_ARGS__)
 #define EUREKA_DISABLE_OPTIMIZATIONS() 
 #define EUREKA_REENABLE_OPTIMIZATIONS() 
 #endif
+
+// NOLINTEND(*)
